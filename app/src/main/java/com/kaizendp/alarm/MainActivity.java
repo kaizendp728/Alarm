28cebe25417ba3e857ea44b715c0df7d7f92b665
kaizendp728 <43077993+kaizendp728@users.noreply.github.com>
2018-10-05 00:34:34 +0700

    Update Code

---
+++ app/src/main/java/com/kaizendp/alarm/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kaizendp.alarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost;
+    }
+}
